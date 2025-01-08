11608964eaccb9603aa77073da11fc3402bb84eb
VictorDuart <victorhd2014@gmail.com>
2025-01-08 19:35:25 -0300

    home page finished

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './HomePage.css';
 import logo from './logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFutbol } from '@fortawesome/free-solid-svg-icons'
 
 class HomePage extends React.Component {
 render() {
@@ -79,6 +81,49 @@ render() {
               <div class="text-right">4.90</div>
             </button>
           </div>
+        </div>
+      </div>
+      <div className="main-container">
+        <h1 className="title"><FontAwesomeIcon icon={faFutbol} /> Jogos ao Vivo</h1>
+        <div className="game-container">
+        <div className="game">
+        <h3>Hoje | 16:00</h3>
+          <h2 class="time1">Time 1</h2>
+          <h2 class="time2">Time 2</h2>
+          <div className="inner-card-container">
+            <button className="inner-card">
+              <div class="text-left">1</div>
+              <div class="text-right">1.19</div>
+            </button>
+            <button className="inner-card">
+              <div class="text-left">X</div>
+              <div class="text-right">2.50</div>
+            </button>
+            <button className="inner-card">
+              <div class="text-left">2</div>
+              <div class="text-right">4.90</div>
+            </button>
+          </div>
+        </div>
+        <div className="game">
+        <h3>Hoje | 16:00</h3>
+          <h2 class="time1">Time 1</h2>
+          <h2 class="time2">Time 2</h2>
+          <div className="inner-card-container">
+            <button className="inner-card">
+              <div class="text-left">1</div>
+              <div class="text-right">1.19</div>
+            </button>
+            <button className="inner-card">
+              <div class="text-left">X</div>
+              <div class="text-right">2.50</div>
+            </button>
+            <button className="inner-card">
+              <div class="text-left">2</div>
+              <div class="text-right">4.90</div>
+            </button>
+          </div>
+        </div>
         </div>
       </div>
       </main>
